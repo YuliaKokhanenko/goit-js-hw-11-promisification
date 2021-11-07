9231d322a9ad21d374be8fd7f257ce23ac3957be
Yulia Kokhanenko <tarutina-z.julia@ukr.net>
2021-11-07 20:51:17 +0200

    add

---
+++ task-2.js
@@ -0,0 +1,28 @@
+/*Перепиши функцию toggleUserState()так, чтобы она не использовала функцию обратного вызова callback, как принимала все два варианта allUsersи userNameи верала промис.*/
+
+const users = [
+  { name: "Mango", active: true },
+  { name: "Poly", active: false },
+  { name: "Ajax", active: true },
+  { name: "Lux", active: false },
+];
+
+const toggleUserState = (allUsers, userName) => {
+  return new Promise((resolve, reject) => {
+    const updatedUsers = allUsers.map((user) =>
+      user.name === userName ? { ...user, active: !user.active } : user
+    );
+    try {
+      resolve(updatedUsers);
+    } catch (error) {
+      reject(console.error(error));
+    }
+  });
+  //   callback(updatedUsers);
+};
+
+const logger = (updatedUsers) => console.table(updatedUsers);
+
+/*Должно работать так */
+toggleUserState(users, "Mango").then(logger);
+toggleUserState(users, "Lux").then(logger);
